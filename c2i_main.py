c108337a195df305c74138a39b508e71e23fd943
Yagnesh <yagneshb@ee.iitb.ac.in>
2018-05-05 12:27:29 +0530

    Added comments

--- c2i_main.py
+++ c2i_main.py
@@ -5,37 +5,39 @@ import pickle
 import random
 from matplotlib import pyplot as plt
 
-beta=0.3
-keep_prob=0.7
-noise_length=100
-batch_size=128
-dataset=open('/home/SharedData/yagnesh/CUB/list.txt','r').read().splitlines()
-epoch=1
-epochs=200000
-lam=2.0
 
+beta=0.3 #Leak Factor
+keep_prob=0.7 #Dropout layer parameter
+noise_length=100 #Noise vector length
+batch_size=128 # Batch Size for training
+dataset=open('/home/SharedData/yagnesh/CUB/list.txt','r').read().splitlines() #List of all images
+epoch=1 #current training iteration
+epochs=255000 #upper limit to the iterations
+lam=2.0 #divergence loss lagrange multiplier
 
-# In[6]:
-
-
+#Function to generate a batch of images and corresponding encoded sentences tensor
 def generate_cub_batch(batch_size):
-    batch=random.sample(dataset,batch_size)
-    real_images=np.empty(shape=[batch_size,64,64,3])
-    encoded_sentence=np.empty(shape=[batch_size,4800])
+    batch=random.sample(dataset,batch_size) #Sample from images at random
+    real_images=np.empty(shape=[batch_size,64,64,3]) #For reading groundtruth images
+    encoded_sentence=np.empty(shape=[batch_size,4800]) #For reading encoded sentences
     for i in range(len(batch)):
         real_images[i]=plt.imread('/home/SharedData/yagnesh/CUB/images_64/'+batch[i]+'.png')
         sentence_list=np.load('/home/SharedData/yagnesh/CUB/encoded_captions/'+batch[i]+'.npy')
         encoded_sentence[i]=sentence_list[random.randint(0,np.shape(sentence_list)[0]-1)]
     return real_images,encoded_sentence
 
+#Setting up GPU parameters
 config = tf.ConfigProto()
 config.gpu_options.allow_growth=True
 config.allow_soft_placement=True
 sess=tf.Session(config=config)
 
+
+#Generator Network
 def Generator(batch_size,z_len,encoded_sentences_tensor,reuse_flag,training_flag):
     with tf.variable_scope(tf.get_variable_scope(),reuse=reuse_flag):
 
+        #Learning mean and variance of the latent conditioning variable
         g_mu_w_0=tf.get_variable('g_mu_w_0',shape=[4800,128],initializer=tf.contrib.layers.xavier_initializer())
         g_mu_b_0=tf.get_variable('g_mu_b_0',shape=[128],initializer=tf.contrib.layers.xavier_initializer())
         g_mu_a_0=tf.matmul(encoded_sentences_tensor,g_mu_w_0)+g_mu_b_0
@@ -46,12 +48,15 @@ def Generator(batch_size,z_len,encoded_sentences_tensor,reuse_flag,training_flag
         g_sd_a_0=tf.matmul(encoded_sentences_tensor,g_sd_w_0)+g_sd_b_0
         g_sd_a_0=tf.maximum(g_sd_a_0,beta*g_sd_a_0)
 
+        #Sampling from latent conditioning variable
         epsilon=tf.random_normal([batch_size,128],mean=0,stddev=1)
         g_cond=g_mu_a_0+tf.multiply(g_sd_a_0,epsilon)
 
+        #Sampling from latent noise variable
         z=tf.random_normal([batch_size,z_len],mean=0,stddev=1)
         ip=tf.concat([z,g_cond],1)
 
+        #Decoder network
         g_w1=tf.get_variable('g_w1',shape=[z_len+128,4*4*1024],initializer=tf.contrib.layers.xavier_initializer())
         g_b1=tf.get_variable('g_b1',shape=[4*4*1024],initializer=tf.contrib.layers.xavier_initializer())
         g_a1=tf.matmul(ip,g_w1)+g_b1
@@ -83,15 +88,19 @@ def Generator(batch_size,z_len,encoded_sentences_tensor,reuse_flag,training_flag
 
         return tf.nn.sigmoid(g_a5),g_mu_a_0,g_sd_a_0
 
+
+#Discriminator Network
 def Discriminator(image_tensor,encoded_sentences_tensor,reuse_flag):
     with tf.variable_scope(tf.get_variable_scope(),reuse=reuse_flag):
 
+        #Caption encoder
         d_w0=tf.get_variable('d_w0',shape=[4800,4*4*128],initializer=tf.contrib.layers.xavier_initializer())
         d_b0=tf.get_variable('d_b0',shape=[4*4*128],initializer=tf.contrib.layers.xavier_initializer())
         d_a0=tf.matmul(encoded_sentences_tensor,d_w0)+d_b0
         d_a0=tf.maximum(d_a0,beta*d_a0)
         d_a0=tf.reshape(d_a0,shape=[batch_size,4,4,128])
 
+        #Encoder network
         d_w1=tf.get_variable('d_w1',shape=[3,3,3,64],initializer=tf.contrib.layers.xavier_initializer())
         d_b1=tf.get_variable('d_b1',shape=[64],initializer=tf.contrib.layers.xavier_initializer())
         d_a1=tf.nn.conv2d(image_tensor,d_w1,strides=[1,2,2,1],padding='SAME')+d_b1
@@ -116,6 +125,7 @@ def Discriminator(image_tensor,encoded_sentences_tensor,reuse_flag):
         d_a4=tf.maximum(d_a4,beta*d_a4)
         d_a4=tf.nn.dropout(d_a4,keep_prob)
 
+        #Augmenting condition
         d_a4=tf.concat([d_a4,d_a0],axis=3)
 
         d_w5=tf.get_variable('d_w5',shape=[4,4,512+128,1024],initializer=tf.contrib.layers.xavier_initializer())
@@ -131,56 +141,71 @@ def Discriminator(image_tensor,encoded_sentences_tensor,reuse_flag):
 
         return d_a6
 
+#Defining placeholders
 encoded_sentences_tensor=tf.placeholder(tf.float32,shape=[batch_size,4800])
 real_images_tensor=tf.placeholder(tf.float32,shape=[batch_size,64,64,3])
 arbit_sentences_tensor=tf.placeholder(tf.float32,shape=[batch_size,4800])
 training_flag = tf.placeholder(tf.bool)
 
+#Generating images from sentence embeddings
 generated_images_tensor,mu1,sd1=Generator(batch_size,noise_length,encoded_sentences_tensor,False,training_flag)
+#Discrminating generated images, real images with real descriptions and real images with unmatched descriptions
 Dg=Discriminator(generated_images_tensor,encoded_sentences_tensor,False)
 Dx=Discriminator(real_images_tensor,encoded_sentences_tensor,True)
-Df=Discriminator(real_images_tensor,arbit_sentences_tensor,True)
+Db=Discriminator(real_images_tensor,arbit_sentences_tensor,True)
 
+#Cross entropy loss for generator
 W_g_ce=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg,labels=tf.ones_like(Dg)-0.1))
+#KLD loss for conditioning latent variable
 N_1=tf.distributions.Normal(mu1,sd1)
 N_1_n=tf.distributions.Normal(tf.zeros([1,128]),tf.ones([1,128]))
 W_g_kl=tf.reduce_mean(tf.contrib.distributions.kl_divergence(N_1,N_1_n))
 W_g=W_g_ce+lam*W_g_kl
 
+#Cross entropy loss for discriminator
 W_d_real=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dx,labels=tf.ones_like(Dx)-0.1))
 W_d_fake=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Dg,labels=tf.zeros_like(Dg)+0.1))
-W_d_bad=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Df,labels=tf.zeros_like(Df)+0.1))
+W_d_bad=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=Df,labels=tf.zeros_like(Db)+0.1))
 W_d=W_d_real+W_d_fake+W_d_bad
 
+#Segregating generator and discriminator variables
 tvars = tf.trainable_variables()
 d_vars = [var for var in tvars if 'd_' in var.name]
 g_vars = [var for var in tvars if 'g_' in var.name]
 var_names=[var.name for var in tf.trainable_variables()]
 
+#Defining optimizers
 update_ops=tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.control_dependencies(update_ops):
     d_trainer=tf.train.AdamOptimizer(25e-6,beta1=0.5).minimize(W_d,var_list=d_vars)
     g_trainer=tf.train.AdamOptimizer(25e-6,beta1=0.5).minimize(W_g, var_list=g_vars)
 
+#Initialize networks and define saver
 sess.run(tf.global_variables_initializer())
 saver=tf.train.Saver()
 # saver.restore(sess,'/home/SharedData/yagnesh/c2i/saved_model_stage1/c2i-'+str(epoch))
 
+#Training code
 while epoch<=epochs:
+    #Sampling mini batch
     real_images,encoded_sentences=generate_cub_batch(batch_size)
     _,arbit_sentences=generate_cub_batch(batch_size)
+
+    #Discriminator update
     _,dLoss=sess.run([d_trainer,W_d],feed_dict={real_images_tensor:real_images,
                                                 encoded_sentences_tensor:encoded_sentences,
                                                 arbit_sentences_tensor:arbit_sentences,
                                                 training_flag:True})
-
+    #Sampling mini batch
     real_images,encoded_sentences=generate_cub_batch(batch_size)
+    #Generator update
     _,gLoss=sess.run([g_trainer,W_g],feed_dict={real_images_tensor:real_images,
                                                 encoded_sentences_tensor:encoded_sentences,
                                                 training_flag:True})
 
     print(epoch,gLoss,dLoss)
 
+    #Saving model and images
     if epoch%100==0:
         saver.save(sess,'/home/SharedData/yagnesh/c2i/saved_model_stage1/c2i',global_step=epoch)
         gen_images=sess.run(generated_images_tensor,feed_dict={real_images_tensor:real_images,
